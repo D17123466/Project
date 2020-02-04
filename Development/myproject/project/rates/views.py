e87b002e16fcc198ab4cfed00641afb6a5ad5b3d
Delfin99 <wjdtmdrl12@gmail.com>
2020-02-04 19:15:36 +0000

    Django - mapping rates to browser & currency converter

--- Development/myproject/project/rates/views.py
+++ Development/myproject/project/rates/views.py
@@ -1,17 +1,44 @@
 from django.shortcuts import render
 
+# Rates
 from datetime import datetime
 import requests
 
+# Converter
+from forex_python.converter import CurrencyRates
+
 # Create your views here.
+
+# Rates API
 url = 'https://api.ratesapi.io/api/latest'
+# url = 'https://api.exchangeratesapi.io/latest'
+# url = 'https://data.fixer.io/api/latest'
 
 def rates(request):
     # Making our request
     response = requests.get(url)
     data = response.json()
-    today = datetime.strptime(data['date'], "%Y-%M-%d").date()
-    today = today.strftime("%d / %b / %Y")
-    return render(request, 'home.html', {'base': data['base'], 'date': today, 'rates': data['rates']})
+
+    # Base currency (Euro)
+    base = data['base']
+
+    # Current date
+    date = datetime.strptime(data['date'], "%Y-%M-%d").date()
+    date = date.strftime("%d / %b / %Y")
+
+    # All currencies
+    rates = data['rates'].items()
+
+    # Converter
+    c = CurrencyRates()
+    conversion_rate = c.convert('EUR', 'KRW', 0)
+
+    # Mapping to template (home.html)
+    return render(request, 'home.html', {'base': base, 'date': date, 'rates': rates, 'conversion_rate': conversion_rate})
     # return render(request, 'home.html', {'rates' :CurrencyRates().get_rates('EUR', date.today())})
 
+
+# def converter(request):
+#     c = CurrencyRates()
+#     conversion_rate = c.convert('EUR', 'KRW', 30000)
+#     return render(request, 'home.html', { 'conversion_rate': conversion_rate})
