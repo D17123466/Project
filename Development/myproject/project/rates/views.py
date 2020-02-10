6a2ac7823e3e04885da297de60f6df778abbf230
Delfin99 <wjdtmdrl12@gmail.com>
2020-02-10 17:37:53 +0000

    Various APIs

--- Development/myproject/project/rates/views.py
+++ Development/myproject/project/rates/views.py
@@ -7,12 +7,18 @@ import requests
 # Converter
 from forex_python.converter import CurrencyRates
 
+import decimal
+
 # Create your views here.
 
 # Rates API
 url = 'https://api.ratesapi.io/api/latest'
 # url = 'https://api.exchangeratesapi.io/latest'
 # url = 'https://data.fixer.io/api/latest'
+# url = 'https://api.exchangerate-api.com/v4/latest/EUR'
+
+# url = "https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol=EUR&to_symbol=USD&interval=5min&apikey=TOPT2TECD5MVTH6P"
+# main_url = base_url + "&from_currency =" + from_currency + "&to_currency =" + to_currency + "&apikey =" + api_key 
 
 def rates(request):
     # Making our request
@@ -23,7 +29,7 @@ def rates(request):
     base = data['base']
 
     # Current date
-    date = datetime.strptime(data['date'], "%Y-%M-%d").date()
+    date = datetime.strptime(data['date'], "%Y-%m-%d").date()
     date = date.strftime("%d / %b / %Y")
 
     # All currencies
@@ -31,15 +37,20 @@ def rates(request):
 
     # Converter
     if (request.GET):
-        print(request.GET['amount'])
-        print(request.GET['from'])
-        print(request.GET['to'])
+        amount = request.GET['amount']
+        from_ = request.GET['from']
+        to_ = request.GET['to']
 
-    c = CurrencyRates()
-    conversion_rate = c.convert('EUR', 'KRW', 0)
+        c = CurrencyRates(force_decimal=True)
+        conversion_rate = c.convert(from_, to_, decimal.Decimal(amount))
+
+        return render(request, 'base.html', {'base': base, 'date': date, 'rates': rates, 'conversion_rate': conversion_rate, 'from': from_, 'to': to_, 'amount': amount})
+
+    # c = CurrencyRates()
+    # conversion_rate = c.convert('EUR', 'KRW', 0)
 
     # Mapping to template (home.html)
-    return render(request, 'base.html', {'base': base, 'date': date, 'rates': rates, 'conversion_rate': conversion_rate})
+    return render(request, 'base.html', {'base': base, 'date': date, 'rates': rates})
     # return render(request, 'home.html', {'rates' :CurrencyRates().get_rates('EUR', date.today())})
 
 
