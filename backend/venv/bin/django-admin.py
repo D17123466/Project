b661c794cbf46c40331f098cb1bcb0603120f0b1
Delfin99 <wjdtmdrl12@gmail.com>
2020-03-16 17:24:39 +0000

    Middle work

--- backend/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/jeong/Tudublin/Project/Final/Development/Project/backend/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
