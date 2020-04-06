82eebaba7894c0c9c09f61a97f955e9a8d12c855
Delfin99 <wjdtmdrl12@gmail.com>
2020-04-06 21:15:02 +0100

    Modified docker configuration files

--- Deployment/docker-run.sh
+++ Deployment/docker-run.sh
@@ -1,6 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y docker.io
+sudo apt-get install -y docker.io docker-compose
 
-# cd /home/ubuntu/Project/Deployment
-docker-compose build
-docker-compose up
+sudo docker-compose build
+sudo docker-compose up
